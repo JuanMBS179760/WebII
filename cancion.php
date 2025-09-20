17f2656e3ce676e52e4cf30c92ac778691399bbd
JuanMa <125672316+JuanMBS179760@users.noreply.github.com>
2025-09-19 21:50:59 -0600

    Add files via upload

---
+++ cancion.php
@@ -0,0 +1,54 @@
+<html> 
+<head> 
+<title></title> 
+</head> 
+<body> 
+<h1>Cancion</h1> 
+<p> 
+<?php 
+echo "Nunca pedí que hicieras eso por mí<br>\n";
+echo "Fue lo que dijo ella antes de partir<br>\n";
+echo "Mi corazón dice que voy a morir<br>\n";
+echo "Sé bien que puedo más no quiero estar así<br>\n";
+
+echo "¿Qué tan bueno será dejarnos ir?<br>\n";
+echo "¿Para qué lastimarnos? Si al final entenderás<br>\n";
+
+echo "Que en las estrellas nuestros nombres<br>\n";
+echo "Siempre se van a encontrar<br>\n";
+echo "Que no puedo dejarte de adorar flaca y te vas<br>\n";
+echo "Pero con suerte nos veremos en el próximo big bang<br>\n";
+
+echo "Que lo mismo me da<br>\n";
+echo "Si me causa ansiedad<br>\n";
+echo "Que yo quiero contigo<br>\n";
+echo "Todo lo vivido y un poquito más<br>\n";
+
+echo "¿Qué chingados gritas?<br>\n";
+echo "Que no te dejo de amar<br>\n";
+echo "Que todas las mentiras que yo me he creído<br>\n";
+echo "Se vuelvan verdad<br>\n";
+echo "El cielo no se ve igual<br>\n";
+echo "Tu risa se va a apagar<br>\n";
+echo "Nos tocará otra vez llorar<br>\n";
+echo "Y no te dejaré de amar<br>\n";
+
+echo "¿Para qué lastimarnos? Si al final entenderás<br>\n";
+echo "Que en las estrellas nuestros nombres<br>\n";
+echo "Siempre se van a encontrar<br>\n";
+echo "Que no puedo dejarte de adorar flaca y te vas<br>\n";
+echo "Pero con suerte nos veremos en el próximo big bang<br>\n";
+
+echo "En el siguiente, en el siguiente será<br>\n";
+echo "Y yo te espero<br>\n";
+echo "Nunca te dije toda la verdad<br>\n";
+echo "Y aunque está mal<br>\n";
+echo "Esperaré volvernos a encontrar<br>\n";
+echo "Nunca te dije toda la verdad flaca y verás<br>\n";
+echo "Que con mis manos y tu piel la Luna nos envidiará<br>\n";
+echo "Quizá con suerte nos veremos en el próximo big bang<br>\n";
+echo "Pero con suerte esperaremos que este no sea el final<br>\n";
+?> 
+</p> 
+</body> 
+</html> 
